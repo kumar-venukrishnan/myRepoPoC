452a925f49600a22c610629da2ec803e1e954634
Kumar Venukrishnan <kumar.venukrishnan@gmail.com>
2021-09-26 07:34:52 +0800

    first commit after jenkins integration

--- myJavaUtils/src/com/cicd/utils/Welcome.java
+++ myJavaUtils/src/com/cicd/utils/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {		
 		System.out.println("Welcome to DevSecOps !!!!!");
+		System.out.println("GitHub - Jenkins Integration is Succesful !!!!!");
 	}
 
 }
