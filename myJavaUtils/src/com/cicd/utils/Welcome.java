e662efe7e709ba11d4132978923eb894abf9bf4e
Kumar Venukrishnan <kumar.venukrishnan@gmail.com>
2021-09-25 09:09:01 +0800

    c

--- myJavaUtils/src/com/cicd/utils/Welcome.java
+++ myJavaUtils/src/com/cicd/utils/Welcome.java
@@ -3,7 +3,7 @@ package com.cicd.utils;
 public class Welcome {
 
 	public static void main(String[] args) {		
-		System.out.println("Welcome to DevSecOps !!!!");
+		System.out.println("Welcome to DevSecOps !!!!!");
 	}
 
 }
