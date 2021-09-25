07ca109c315fa09e38d41e249103a09bfd929055
Kumar Venukrishnan <kumar.venukrishnan@gmail.com>
2021-09-26 07:38:57 +0800

    2

--- myJavaUtils/src/com/cicd/utils/Welcome.java
+++ myJavaUtils/src/com/cicd/utils/Welcome.java
@@ -4,7 +4,8 @@ public class Welcome {
 
 	public static void main(String[] args) {		
 		System.out.println("Welcome to DevSecOps !!!!!");
-		System.out.println("GitHub - Jenkins Integration is Succesful 1 !!!!!");
+		System.out.println("GitHub - Jenkins Integration is Succesful 2 !!!!!");
+		System.out.println("Next Step to build a jar !!!!!");
 	}
 
 }
